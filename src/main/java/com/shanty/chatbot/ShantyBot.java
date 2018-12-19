adbdb3aa0581cc022675f10847598d968ba5bf5b
outofmind <wofffk@gmail.com>
2018-12-19 12:03:07 +0530

    removing token from VCS; adding some base functionality

--- src/main/java/com/shanty/chatbot/ShantyBot.java
+++ src/main/java/com/shanty/chatbot/ShantyBot.java
@@ -4,9 +4,33 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
 public class ShantyBot extends TelegramLongPollingBot {
+    private static String token;
+
+    static {
+        InputStream tokenStream = ShantyBot.class.getClassLoader().getResourceAsStream("_token");
+        if (tokenStream == null) {
+            throw new IllegalArgumentException("Cannot find _token file");
+        }
+
+        try (BufferedReader is = new BufferedReader(new InputStreamReader(tokenStream))) {
+            token = is.lines().collect(Collectors.joining());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
     public void onUpdateReceived(Update update) {
         if (!update.hasMessage()) {
             return;
@@ -19,10 +43,37 @@ public class ShantyBot extends TelegramLongPollingBot {
 
         String messageText = updateMessage.getText();
         long chatId = updateMessage.getChatId();
+        SendMessage message = new SendMessage().setChatId(chatId);
+        switch (messageText) {
+            case "/start":
+                message.setText("Привет. Я твой помощник по комфортному проживанию в Шанти вилле. Чем могу помочь?");
+                ReplyKeyboardMarkup keyboardMarkup = new ShantyKeyboard();
+                message.setReplyMarkup(keyboardMarkup);
+                break;
 
-        SendMessage message = new SendMessage()
-                .setChatId(chatId)
-                .setText(messageText);
+            /*case "/hideMarkup":
+                message.setText("Keyboard are hidden now");
+                message.setReplyMarkup(new ReplyKeyboardRemove());
+                break;*/
+            case "FAQ":
+                message.setText("https://telegra.ph/FAQ-poleznye-ssylki-10-25");
+                break;
+            case "About":
+                message.setText("https://telegra.ph/SHANTI-VILLA-10-26");
+                break;
+            case "Help":
+                message.setText("Когда нибудь я обязательно помогу тебе! Но не сегодня...");
+                break;
+            /*case "Привет": {
+                SendSticker sticker = new SendSticker();
+                sticker.
+                break;
+            }*/
+
+            default:
+                message.setText("Unknown command: " + messageText);
+        }
+
 
         try {
             execute(message);
@@ -31,11 +82,13 @@ public class ShantyBot extends TelegramLongPollingBot {
         }
     }
 
+    @Override
     public String getBotUsername() {
         return "ShantyBot";
     }
 
+    @Override
     public String getBotToken() {
-        return "743975544:AAHWS_WQXJlBX8XWXOSJx8QCuRU_xqrW1mg";
+        return token;
     }
 }
---
+++ src/main/java/com/shanty/chatbot/ShantyKeyboard.java
@@ -0,0 +1,42 @@
+package com.shanty.chatbot;
+
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+
+class ShantyKeyboard extends ReplyKeyboardMarkup {
+    ShantyKeyboard() {
+
+        Properties properties = new Properties();
+        try (InputStream is = ShantyKeyboard.class.getClassLoader().getResourceAsStream("keyboard.properties")) {
+            properties.load(is);
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot read keyboard.properties file", e);
+        }
+
+        List<KeyboardRow> keyboard = new ArrayList<>();
+        Set<String> propertyNames = properties.stringPropertyNames();
+        Integer lastRowProcessed = null;
+        KeyboardRow currentRow = new KeyboardRow();
+        keyboard.add(currentRow);
+        for (String key : propertyNames) {
+            String[] rowCellArr = key.split("\\.");
+            Integer rowIndex = Integer.parseInt(rowCellArr[0].replace("row", ""));
+
+            if (!rowIndex.equals(lastRowProcessed)) {
+                lastRowProcessed = rowIndex;
+                currentRow = new KeyboardRow();
+                keyboard.add(currentRow);
+            }
+            currentRow.add(properties.getProperty(key));
+        }
+
+        setKeyboard(keyboard);
+    }
+}
